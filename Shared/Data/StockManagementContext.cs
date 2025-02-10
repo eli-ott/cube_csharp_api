9b5a374918db0b3b8817b7cbccc31b5ef30ddfc1
Eliott BIDAULT-HERVOUET <github.wavy095@passmail.net>
2025-02-10 14:29:21 +0100

    Added db context

--- Shared/Data/StockManagementContext.cs
+++ Shared/Data/StockManagementContext.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using MonApi.API.Addresses.Models;
+using MonApi.API.Customers.Models;
+using MonApi.API.Passwords.Models;
 using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
 
 namespace MonApi.Models;
@@ -53,8 +56,13 @@ public partial class StockManagementContext : DbContext
     public virtual DbSet<SupplierOrderLine> SupplierOrderLines { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;database=stock_management;user id=root;pooling=true", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.4.32-mariadb"));
+    {
+        string connectionString = Environment.GetEnvironmentVariable("DATABASE_CONNECTION_STRING")
+                                  ?? throw new InvalidOperationException(
+                                      "Connection string 'DATABASE_CONNECTION_STRING' not found.");
+
+        optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
