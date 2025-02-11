a8a025b31b0c9889a0fb4ebc9557896bb7a46cdf
Eliott BIDAULT-HERVOUET <github.wavy095@passmail.net>
2025-02-11 12:52:13 +0100

    Added new context and new customer

---
+++ API/Customers/Models/Customer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using MonApi.API.Addresses.Models;
+using MonApi.API.Passwords.Models;
+using MonApi.Models;
+
+namespace MonApi.API.Customers.Models;
+
+public partial class Customer
+{
+    public int CustomerId { get; set; }
+
+    public string LastName { get; set; } = null!;
+
+    public string FirstName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Phone { get; set; } = null!;
+
+    public DateTime? DeletionTime { get; set; }
+
+    public bool Active { get; set; }
+
+    public string ValidationId { get; set; } = null!;
+
+    public DateTime CreationTime { get; set; }
+
+    public DateTime UpdateTime { get; set; }
+
+    public int PasswordId { get; set; }
+
+    public int AddressId { get; set; }
+
+    public virtual Address Address { get; set; } = null!;
+
+    public virtual ICollection<Cart> Carts { get; set; } = new List<Cart>();
+
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+
+    public virtual Password Password { get; set; } = null!;
+}
