012e962f359c3de37018bed6ece808500571e8f4
Gosselin Noa <no.gosselin2006@gmail.com>
2025-02-12 19:47:58 +0100

    Implement pagination for family retrieval with query parameters

--- API/Families/Services/IFamiliesService.cs
+++ API/Families/Services/IFamiliesService.cs
@@ -1,4 +1,6 @@
+using MonApi.API.Families.Filters;
 using MonApi.API.Families.Models;
+using MonApi.Shared.Pagination;
 
 namespace MonApi.API.Families.Services
 {
@@ -7,7 +9,7 @@ namespace MonApi.API.Families.Services
         public Task<Family> AddAsync(Family family);
         public Task<Family> UpdateAsync(int id, Family family);
         public Task<Family> FindById(int id);
-        public Task<List<Family>> GetAll();
+        public Task<PagedResult<Family>> GetAll(FamilyQueryParameters familyQueryParameters);
         public Task<Family> SoftDeleteAsync(int id);
     }
 }
