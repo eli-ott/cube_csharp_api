9a539ee005e6540dbd040f380c55c9629c656eab
Titouan Massé <titouanmasselaude@gmail.com>
2025-02-12 14:34:06 +0100

    fix onconfiguring et base repo :zap:

--- Shared/Data/StockManagementContext.cs
+++ Shared/Data/StockManagementContext.cs
@@ -61,8 +61,12 @@ public partial class StockManagementContext : DbContext
     public virtual DbSet<SupplierOrderLine> SupplierOrderLines { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;database=stock_management;user id=root;pooling=true", ServerVersion.Parse("10.4.32-mariadb"));
+    {
+        string connectionString = Environment.GetEnvironmentVariable("DATABASE_CONNECTION_STRING")
+                                  ?? throw new InvalidOperationException(
+                                      "Connection string 'DATABASE_CONNECTION_STRING' not found.");
+        optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
