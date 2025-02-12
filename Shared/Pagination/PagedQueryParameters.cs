f555fda2f5552410b61cad983779385c7e877003
Gosselin Noa <no.gosselin2006@gmail.com>
2025-02-12 19:47:43 +0100

    Add pagination classes for handling paged queries and results

---
+++ Shared/Pagination/PagedQueryParameters.cs
@@ -0,0 +1,16 @@
+﻿namespace MonApi.Shared.Pagination
+{
+    public class PagedQueryParameters
+    {
+        private const int MaxPageSize = 50;
+        private int _pageSize = 10;
+
+        // Pagination properties
+        public int page { get; set; } = 1;
+        public int size
+        {
+            get => _pageSize;
+            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+        }
+    }
+}
